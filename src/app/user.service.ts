b131f2b4b61b59457c3f2ff92c273531e3b88d8e
wafaamuhammad123 <wafaa.muhammad2000@gmail.com>
2024-05-19 22:42:31 +0300

    adding caching mechanism

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,29 +1,64 @@
-// src/app/user.service.ts
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { delay } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, tap, catchError, delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private baseUrl = 'https://reqres.in/api/users';
+  private usersCache: { [key: number]: any } = {};
+  private userDetailCache: { [key: number]: any } = {};
 
   constructor(private http: HttpClient) { }
 
   getUsers(page: number): Observable<any> {
+    // Check if data is already in the cache
+    if (this.usersCache[page]) {
+      console.log(`Fetching users for page ${page} from cache`);
+      return of(this.usersCache[page]);
+    }
+
+    console.log(`Fetching users for page ${page} from network`);
     return this.http.get<any>(`${this.baseUrl}?page=${page}`).pipe(
-      delay(1000)  //add delay to test spinner
+      tap(response => {
+        console.log(`Caching users for page ${page}`);
+        this.usersCache[page] = response;
+      }),
+      catchError(error => {
+        console.error('Error fetching users:', error);
+        throw error;
+      })
+      ,delay(1000)
     );
   }
 
   getUserById(userId: number): Observable<any> {
+    // Check if data is already in the cache
+    if (this.userDetailCache[userId]) {
+      console.log(`Fetching user details for user ID ${userId} from cache`);
+      return of(this.userDetailCache[userId]);
+    }
+
+    console.log(`Fetching user details for user ID ${userId} from network`);
     return this.http.get<any>(`${this.baseUrl}/${userId}`).pipe(
-      delay(1000),  
-      map(response => response.data)
+      map(response => response.data),
+      tap(user => {
+        console.log(`Caching user details for user ID ${userId}`);
+        this.userDetailCache[userId] = user;
+      }),
+      
+      catchError(error => {
+        console.error(`Error fetching user details for user ID ${userId}:`, error);
+        throw error;
+      }), 
+      delay(1000)// to test spinner
     );
+  }
+
+  clearCache(): void {
+    this.usersCache = {};
+    this.userDetailCache = {};
   }
 }
